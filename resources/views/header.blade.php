218af6f2b1cc2a808077a9af8760557f598209ab
ginaa14 <gina.ekaristi@si.ukdw.ac.id>
2023-06-05 15:41:51 +0700

    update 3

---
+++ resources/views/header.blade.php
@@ -0,0 +1,35 @@
+
+<header class="site-header js-site-header">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-6 col-lg-4 site-logo" data-aos="fade">
+            <a href="/"><img src="images/logo.png" style="width:105px;height:100px;margin:30px;"></a></div>
+          <div class="col-6 col-lg-8">
+
+            <div class="site-menu-toggle js-site-menu-toggle"  data-aos="fade">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <!-- END menu-toggle -->
+
+            <div class="site-navbar js-site-navbar">
+              <nav role="navigation">
+                <div class="container">
+                  <div class="row full-height align-items-center">
+                    <div class="col-md-6 mx-auto">
+                      <ul class="list-unstyled menu">
+                        <li class="active"><a href="/">Home</a></li>
+                        <li><a href="/room">Rooms</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
